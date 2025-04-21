5e49c9071d191334f74abd4aea2ab9fb44a98f27
tmzfj12 <tmzjf12@naver.com>
2025-04-21 17:07:06 +0900

    구현예정 기능 주석
    구현할 기능과 출력할 화면에 대한 주석

---
+++ Scenes/BattleStart.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team3TextRPG
+{
+    internal class BattleStart
+    {
+        public BattleStart()
+        {
+            //몬스터 1~4마리를 랜덤으로 생성
+            //3종류,중복 가능,표시 순서 랜덤
+            //생성한 몬스터의 정보 출력
+            //플레이어 정보 출력
+            //행동입력
+
+            Console.WriteLine("전투 시작 씬입니다.");
+            Console.WriteLine("Battle!!");
+        }
+        //몬스터 1~4마리를 랜덤으로 생성
+        //생성한 몬스터의 정보 출력
+        //플레이어 정보 출력
+        //행동입력
+        
+    }
+}
