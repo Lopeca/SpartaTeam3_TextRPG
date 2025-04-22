b61668708604710b009852f33692658481e7ac8e
Lopeca <3167ls3@naver.com>
2025-04-22 16:25:01 +0900

    클래스명 변경

--- Scenes/BattleStart.cs
+++ Scenes/BattleStart.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class BattleStart : SceneBase
+public class BattleStartScene : SceneBase
 {
     public override void AddSelections()
     {
