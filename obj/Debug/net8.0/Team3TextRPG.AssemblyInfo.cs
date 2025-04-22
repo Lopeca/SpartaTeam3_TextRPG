2d38c14040c048969196adbe6e81928372f8694c
tmzfj12 <tmzjf12@naver.com>
2025-04-22 15:35:34 +0900

    ..

--- obj/Debug/net8.0/Team3TextRPG.AssemblyInfo.cs
+++ obj/Debug/net8.0/Team3TextRPG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Team3TextRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eb40a53597401e2ba0fd1ce45f4b1b0b2855b1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Team3TextRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Team3TextRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
